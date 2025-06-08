9beaf9aa72639b124ed201512fb2072232406bde
FernandoVS11 <a18000997@alumnos.uady.mx>
2025-06-08 01:32:11 -0600

    changes to the backed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,68 +1,98 @@
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const path = require('path')
+const Person = require('./models/person')
 
-app.use(express.json());
+const app = express()
 
-morgan.token('body', (req) => {
-  return req.method === 'POST' ? JSON.stringify(req.body) : '';
-});
+app.use(cors())
+app.use(express.json())
 
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
-);
+// Morgan personalizado para mostrar body en POST
+morgan.token('body', req => req.method === 'POST' ? JSON.stringify(req.body) : '')
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  { id: 1, name: 'Arto Hellas', number: '040-123456' },
-  { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-  { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-  { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-];
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+})
 
-// Rutas...
-app.get('/api/persons', (req, res) => res.json(persons));
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) res.json(person)
+      else res.status(404).end()
+    })
+    .catch(error => next(error))
+})
 
-app.get('/info', (req, res) => {
-  const info = `
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-  `;
-  res.send(info);
-});
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(p => p.id === id);
-  if (person) res.json(person);
-  else res.status(404).send({ error: 'Person not found' });
-});
-
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(p => p.id !== id);
-  res.status(204).end();
-});
-
-app.post('/api/persons', (req, res) => {
-  const body = req.body;
   if (!body.name || !body.number) {
-    return res.status(400).json({ error: 'name and number are required' });
-  }
-  if (persons.find(p => p.name === body.name)) {
-    return res.status(400).json({ error: 'name must be unique' });
+    return res.status(400).json({ error: 'name and number are required' })
   }
 
-  const newPerson = {
-    id: Math.floor(Math.random() * 1000000),
+  const person = new Person({
     name: body.name,
     number: body.number
-  };
+  })
 
-  persons = persons.concat(newPerson);
-  res.json(newPerson);
-});
+  person.save()
+    .then(savedPerson => res.json(savedPerson))
+    .catch(error => next(error))
+})
 
-const PORT = 3001;
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
+
+  const updatedPerson = {
+    name,
+    number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, updatedPerson, {
+    new: true,
+    runValidators: true,
+    context: 'query'
+  })
+    .then(result => res.json(result))
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => res.status(204).end())
+    .catch(error => next(error))
+})
+
+app.get('/info', (req, res, next) => {
+  Person.countDocuments({}).then(count => {
+    res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`)
+  }).catch(error => next(error))
+})
+
+app.use(express.static('dist'))
+
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).json({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
